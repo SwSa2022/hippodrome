e9865c2284ac553913d5375deaf08cc415de0fcc
Swetlana Sajzewa <ssajzewa@wbsedu.de>
2024-05-03 23:06:28 +0200

    Logger  HippodromeClass

--- src/main/java/Hippodrome.java
+++ src/main/java/Hippodrome.java
@@ -1,3 +1,8 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -5,19 +10,26 @@ import java.util.List;
 import static java.util.Objects.isNull;
 
 public class Hippodrome {
-
+    private static final Logger logger = LogManager.getLogger(Hippodrome.class);
     private final List<Horse> horses;
 
     public Hippodrome(List<Horse> horses) {
         if (isNull(horses)) {
+            logger.error("Horses list is null");
             throw new IllegalArgumentException("Horses cannot be null.");
         } else if (horses.isEmpty()) {
+            logger.error("Horses list is empty");
             throw new IllegalArgumentException("Horses cannot be empty.");
         }
 
         this.horses = horses;
+        logHippodromeCreation();
     }
 
+    private void logHippodromeCreation() {
+        String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS"));
+        logger.debug("Создание Hippodrome, лошадей [{}]", horses.size());
+    }
     public List<Horse> getHorses() {
         return Collections.unmodifiableList(horses);
     }
